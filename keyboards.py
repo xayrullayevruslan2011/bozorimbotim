16a32d12382e1d84afb24fb292b5e4f7d1b8efb7
Ruslan <nurillobegmatov@gmail.com>
2026-03-01 12:43:26 +0300

    bot yangilandi 995

--- keyboards.py
+++ keyboards.py
@@ -24,7 +24,7 @@ from config import ADMIN_USERNAME
 # LINKLAR
 NASIYA_URL = "https://nasiyaruslan.vercel.app"
 PLATFORM_URL = "https://online-kurs-kmkn.vercel.app/"
-SHOP_URL = "https://ruslan-shop-brand-s8n3.vercel.app" # YANGI QO'SHILDI
+SHOP_URL = "https://ruslan-shop-uz.vercel.app/" # YANGI QO'SHILDI
 
 
 # =========================================================
